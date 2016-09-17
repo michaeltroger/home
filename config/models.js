9db30cea6af76b2bb30adc7267e257e1a4fe8955
s1310238069@students.fh-hagenberg.at <michaeltroger@users.noreply.github.com>
2016-09-18 00:05:11 +0200

    added license

--- config/models.js
+++ config/models.js
@@ -1,3 +1,19 @@
+/**
+This file is part of Home Surveillance by Michael Troger.
+
+Home Surveillance by Michael Troger is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Home Surveillance by Michael Troger is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Home Surveillance by Michael Troger.  If not, see <http://www.gnu.org/licenses/>.
+ */
 var mongoose = require('mongoose');
 
 
