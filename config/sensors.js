e7cb5ccdbe09e563373494b51bdb583c09340fbe
s1310238069@students.fh-hagenberg.at <michaeltroger@users.noreply.github.com>
2016-09-18 22:00:26 +0200

    changed (c) and readme

--- config/sensors.js
+++ config/sensors.js
@@ -1,19 +1,20 @@
 /**
-This file is part of Home Surveillance by Michael Troger.
+Copyright 2016 Michael Troger <https://michaeltroger.com>
+This file is part of Home Surveillance with Node.js.
 
-Home Surveillance by Michael Troger is free software: you can redistribute it and/or modify
+Home Surveillance with Node.js is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 3 of the License, or
 (at your option) any later version.
 
-Home Surveillance by Michael Troger is distributed in the hope that it will be useful,
+Home Surveillance with Node.js is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-along with Home Surveillance by Michael Troger.  If not, see <http://www.gnu.org/licenses/>.
- */
+along with Home Surveillance with Node.js.  If not, see <http://www.gnu.org/licenses/>.
+*/
 var sensor1 = '28-000007b1a73d';
 var sensor2 = '28-000007b32d2c';
 
--- readme.md
+++ readme.md
@@ -1,9 +1,13 @@
-Create a file:
 
-config/db_url with the following content (fill out your connection details:
+Install dependencies: `npm install`
+
+Create a file:
+`config/db_url` with the following content (fill out your connection details):
 ```
 var monodbUrl = 'mongodb://<user>:<password>@<server>:<port>/<database>';
 module.exports = monodbUrl;
 ```
 
-config/sensors.js enter here your temperature sensor ids
+`config/sensors.js` enter here your temperature sensor ids
+
+Launch: `node index`
